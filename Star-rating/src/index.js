d57b52cf451d31c02726571bcb5d6f675823eab5
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2024-03-27 20:58:27 +0800

    Add files via upload

---
+++ Star-rating/src/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import StarRating from './StarRating';
+// import './index.css';
+// import App from './App';
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>Rating : {movieRating}</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} defaultRaing={5} />
+    <StarRating maxRating={5} size={30} color="red" className="test" />
+    <Test />
+  </React.StrictMode>
+);
